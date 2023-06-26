9213d7c0d6ca981957908bd2ded56a5217fb9d2f
vadim <ivadimn@mail.ru>
2023-06-26 17:52:08 +0300

    add menu

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -6,6 +6,11 @@ class MainMenu(QMenuBar):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        nsi_menu = self.addMenu("НСИ")
+        self.__pd = nsi_menu.addAction("Персональные данных")
+        self.__goals = nsi_menu.addAction("Цели обработки ПД")
+        self.__category = nsi_menu.addAction("Категории субъектов ПД")
+
         help_menu = self.addMenu("Справка")
         self.__about = help_menu.addAction("О программе...")
         self.__about_qt = help_menu.addAction("О библиотеке Qt...")
